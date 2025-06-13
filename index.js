10f4df2f3eb15fc0d29e47306e6ab772c3dfa2e5
Stella-055 <stellaongayo243@gmail.com>
2025-06-13 20:28:02 +0300

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client";
+const app = express()
+app.use(express.json())
+const prisma = new PrismaClient
+const port= process.env.PORT || 5000
+app.listen(port,()=>{
+
+    console.log("server is up and running")
+})
